a1f94d911fa16f3f5854964ba53d7c01b2785a85
Per Rawdin <pr222iw@student.lnu.se>
2021-04-27 23:23:17 +0200

    Add basic form in Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 
 export default function Register() {
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
     return (
         <div>
-            
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                  label="Username"
+                  placeholder="Username"
+                  name="username"
+                  value={values.username}
+                  onChange={onChange}
+                  />
+                <Form.Input
+                  label="Email"
+                  placeholder="Email"
+                  name="email"
+                  value={values.email}
+                  onChange={onChange}
+                  />
+                <Form.Input
+                  label="Password"
+                  placeholder="Password"
+                  name="password"
+                  value={values.password}
+                  onChange={onChange}
+                  />
+                <Form.Input
+                  label="ConfirmPassword"
+                  placeholder="ConfirmPassword"
+                  name="confirmPassword"
+                  value={values.confirmPassword}
+                  onChange={onChange}
+                  />
+            </Form>
+            <Button type='submit'>Submit</Button>
         </div>
     )
 }
