948ad6a45cabebfdb42ed4888798efdc0760c659
Per Rawdin <pr222iw@student.lnu.se>
2021-04-29 22:27:32 +0200

    Add register gql query

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { BoardList } from './components/BoardList/BoardList'
 import Navbar from './components/NavBar/NavBar';
 import ThreadsList from './components/ThreadList/ThreadList';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
         <Route exact path='/' component={BoardList} />
         <Route exact path='/posts' component={ThreadsList} />
+        <Route exact path='/register' component={Register} />
       </Router>
     </Container>
   );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,48 +1,82 @@
+import { gql, useMutation } from '@apollo/client'
 import React, { useState } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 
-export default function Register() {
-    const [values, setValues] = useState({
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    })
+const REGISTER_USER = gql`
+  mutation register(
+      $username: String!
+      $email: String!
+      $password: String!
+      $confirmPassword: String!
+    ) {
+      register(
+        registerInput: {
+          username: $username
+          email: $email
+          password: $password
+          confirmPassword: $confirmPassword
+        }
+      ) {
+        id
+      }
+    }
+`
 
-    return (
-        <div>
-            <Form /*onSubmit={onSubmit}*/ noValidate>
-                <h1>Register</h1>
-                <Form.Input
-                  label="Username"
-                  placeholder="Username"
-                  name="username"
-                  value={values.username}
-                  // onChange={onChange}
-                  />
-                <Form.Input
-                  label="Email"
-                  placeholder="Email"
-                  name="email"
-                  value={values.email}
-                  // onChange={onChange}
-                  />
-                <Form.Input
-                  label="Password"
-                  placeholder="Password"
-                  name="password"
-                  value={values.password}
-                  // onChange={onChange}
-                  />
-                <Form.Input
-                  label="ConfirmPassword"
-                  placeholder="ConfirmPassword"
-                  name="confirmPassword"
-                  value={values.confirmPassword}
-                  // onChange={onChange}
-                  />
-            </Form>
-            <Button type='submit'>Submit</Button>
-        </div>
-    )
+export default function Register() {
+  const [registerUser] = useMutation(REGISTER_USER)
+
+  const [values, setValues] = useState({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+  })
+
+  const onChange = (event) => {
+    setValues({...values, [event.target.name]: event.target.value})
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    registerUser({variables: values})
+    alert("Registered")
+  }
+
+  return (
+    <div>
+        <Form onSubmit={onSubmit} noValidate>
+            <h1>Register</h1>
+            <Form.Input
+              label="Username"
+              placeholder="Username"
+              name="username"
+              value={values.username}
+              onChange={onChange}
+              />
+            <Form.Input
+              label="Email"
+              placeholder="Email"
+              name="email"
+              value={values.email}
+              onChange={onChange}
+              />
+            <Form.Input
+              label="Password"
+              placeholder="Password"
+              name="password"
+              value={values.password}
+              onChange={onChange}
+              />
+            <Form.Input
+              label="ConfirmPassword"
+              placeholder="ConfirmPassword"
+              name="confirmPassword"
+              value={values.confirmPassword}
+              onChange={onChange}
+              />
+              <Button type='submit'>Submit</Button>
+        </Form>
+        
+    </div>
+  )
 }
