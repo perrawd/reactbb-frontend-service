fce0da63be9d68aa33c57ab3631dd5173efa08c7
Per Rawdin <pr222iw@student.lnu.se>
2021-05-03 10:49:46 +0200

    Add test for Login component

---
+++ src/components/login/__test__/Login.test.js
@@ -0,0 +1,29 @@
+import Login from '../Login'
+import { render, screen, fireEvent } from '@testing-library/react';
+import { ApolloProvider } from "@apollo/client"
+import client from '../../../utils/ApolloClient.js'
+
+test('It renders login page without error', () => {
+  render(
+    <ApolloProvider client={client}>
+      <Login></Login>
+    </ApolloProvider>
+    )
+  const pageElement = screen.getByText(/Login/i);
+  expect(pageElement).toBeInTheDocument();
+})
+
+it('can change the value of a Input', () => {
+  const { getAllByPlaceholderText } = render(
+    <ApolloProvider client={client}>
+      <Login></Login>
+    </ApolloProvider>
+  )
+
+  const element = getAllByPlaceholderText('Username')
+  const elementInput = element[0]
+  
+  fireEvent.change(elementInput, { target: { value: 'mockUser' } })
+  
+  expect(elementInput).toHaveValue('mockUser')
+})
