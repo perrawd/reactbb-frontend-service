c3ae2ac490238b5b46224ca0b445edfbaefabd66
Per Rawdin <pr222iw@student.lnu.se>
2021-05-13 11:43:25 +0200

    Add deletepost query

--- src/pages/EditPost/EditPost.js
+++ src/pages/EditPost/EditPost.js
@@ -9,24 +9,37 @@ const EditPost = props => {
   console.log(props)
 
   const EDIT_POST = gql`
-  mutation editPost(
-      $id: ID!
-      $body: String!
-    ){
-    editPost(
-        id: $id
-        body: $body
-    ){
-      success
+    mutation editPost($id: ID!, $body: String!) {
+      editPost(id: $id, body: $body) {
+        success
+      }
     }
-  }
+  `
+
+  const DELETE_POST = gql`
+    mutation deletePost($id: ID!) {
+      deletePost(id: $id) {
+        success
+      }
+    }
   `
 
   const [postValues, setPostValues] = useState({
-      body: props.location.state.body
+    body: props.location.state.body
   })
 
   const [editPost, { loading }] = useMutation(EDIT_POST, {
+    onCompleted (data) {
+      // eslint-disable-next-line no-console
+      console.log(data)
+    },
+    onError (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
+  })
+
+  const [deletePost] = useMutation(DELETE_POST, {
     onCompleted (data) {
       // eslint-disable-next-line no-console
       console.log(data)
@@ -50,12 +63,18 @@ const EditPost = props => {
 
     editPost({ variables: postValues })
     // eslint-disable-next-line no-console
-    console.log("OK")
+    console.log('OK')
   }
 
-  return (
-    user && user.sub.username === props.location.state.author
-    ? <div>
+  const deleteSubmit = event => {
+    event.preventDefault()
+    deletePost({
+      variables: { id: props.location.state.id }
+    })
+  }
+
+  return user && user.sub.username === props.location.state.author
+  ? <div>
       <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
         <h1>Edit Post</h1>
         <Form.Input
@@ -66,12 +85,13 @@ const EditPost = props => {
           value={postValues.body}
           type="text"
         />
-        <Button primary type="submit">Edit Post</Button>
-        <Button>Delete Post</Button>
+        <Button primary type="submit">
+          Edit Post
+        </Button>
+        <Button basic color="red" onClick={deleteSubmit}>Delete Post</Button>
       </Form>
     </div>
     : <h1>Unauthorized</h1>
-  )
 }
 
 export default EditPost
