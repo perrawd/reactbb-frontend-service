55f8203a73407fa48a09cddbf8329af3e598c9e0
Per Rawdin <pr222iw@student.lnu.se>
2021-05-21 22:01:29 +0200

    Add addCategory

--- src/pages/AdminPage/AdminCategories/AdminCategories.js
+++ src/pages/AdminPage/AdminCategories/AdminCategories.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { gql, useQuery } from '@apollo/client'
-import { Card, Button } from 'semantic-ui-react'
+import { gql, useQuery, useMutation } from '@apollo/client'
+import { Card, Button, Modal, Form } from 'semantic-ui-react'
 import EditCategory from '../EditCategory/EditCategory'
 
 const GET_CATEGORIES_QUERY = gql`
@@ -21,9 +21,19 @@ const GET_CATEGORIES_QUERY = gql`
   }
 `
 
+const ADD_CATEGORY = gql`
+  mutation addCategory($title: String!, $subtitle: String) {
+    addCategory(title: $title, subtitle: $subtitle) {
+      title
+      subtitle
+    }
+  }
+`
+
 const AdminCategories = () => {
   const [activeCategory, setActiveCategory] = useState({})
   const [editCategory, setEditCategory] = useState(false)
+  const [open, setOpen] = React.useState(false)
 
   const { loading, error, data } = useQuery(GET_CATEGORIES_QUERY)
   if (loading) {
@@ -33,9 +43,37 @@ const AdminCategories = () => {
     return `Error! ${error.message}`
   }
 
+  const [addCategory] = useMutation(ADD_CATEGORY, {
+    onCompleted () {
+      // eslint-disable-next-line no-console
+      console.log(data)
+    },
+    onError (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
+  })
+
+  const [postValues, setPostValues] = useState({
+    title: '',
+    subtitle: ''
+  })
+
+  const onPostChange = event => {
+    setPostValues({
+      ...postValues,
+      [event.target.name]: event.target.value
+    })
+  }
+
   const handleOnClick = cat => {
     setActiveCategory(cat)
     setEditCategory(true)
+  }
+
+  const addButton = () => {
+    addCategory({ variables: postValues})
+    setOpen(false)
   }
 
   const categories = data.getCategories
@@ -46,7 +84,46 @@ const AdminCategories = () => {
     editCategory
     ? <EditCategory category={activeCategory} handler={setEditCategory}/>
     : <div>
-    <Button basic color="green" size="small">Add a category</Button>
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={<Button basic color="green" size="small" >Add category</Button>}
+    >
+      <Modal.Header>Add a new category</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Form noValidate className={loading ? 'loading' : ''}>
+        <Form.Input
+          onChange={onPostChange}
+          name="title"
+          label="title"
+          value={postValues.title}
+          type="text"
+        />
+        <Form.Input
+          onChange={onPostChange}
+          name="subtitle"
+          label="subtitle"
+          value={postValues.subtitle}
+          type="text"
+        />
+      </Form>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={() => setOpen(false)}>
+          Nope
+        </Button>
+        <Button
+          content="Add category"
+          labelPosition="right"
+          icon="checkmark"
+          onClick={addButton}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
     <h5>To edit a category and its subcategories, click on Edit.</h5>
 
       <Card.Group>
